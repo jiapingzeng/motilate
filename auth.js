cc6f4548f0a5ec0016ef764a8969be8e295ed705
Gman59 <druckergm@gmail.com>
2018-11-11 05:43:07 -0800

    Made if acesss config vars

--- auth.js
+++ auth.js
@@ -1,12 +1,13 @@
 //All requirements for this file
 var passport = require('passport')
 var Auth0Strategy = require('passport-auth0')
+const aws = require('aws-sdk');
 
 //Instance information needed for authentication
 var strategy = new Auth0Strategy({
-    domain: 'codeday2018fall.auth0.com',
-    clientID: 'Kp90PfE61zYbNvOqK8IF5vGt1h0WDEWa',
-    clientSecret: 'jkorKKZW6iEfuMUm8RufAEvany-J2kh03kB_vRWgVVvdxdoqTh56yuPTxhl5ad-B',
+    domain: process.env.domain,
+    clientID: process.env.cID,
+    clientSecret: process.env.cS,
     callbackURL: 'https://localhost:3000/user/callback'
 }, (accessToken, refreshToken, extraParams, profile, done) => {
     return done(null, profile)
