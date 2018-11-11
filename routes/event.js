bc99d5c33d816a5faed5a6a0ebdd60ef7628bd29
aaron <aahuang@berkeley.edu>
2018-11-11 05:25:50 -0800

    start integration

--- routes/event.js
+++ routes/event.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/', (req, res) => {
-
+router.get('/add', (req, res) => {
+    res.render('create')
 })
 
 module.exports = router
