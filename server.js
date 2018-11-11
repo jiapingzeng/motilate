dab127419a2f089368a3a19273e965da2b1f93d5
Gman59 <druckergm@gmail.com>
2018-11-10 18:44:34 -0800

    Added base of all facebook login. (I think)

--- server.js
+++ server.js
@@ -65,3 +65,12 @@ passport.use(strategy);
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
