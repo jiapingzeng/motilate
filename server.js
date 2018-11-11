f376944b1c6787e244fd8d73399447a4637ee88c
Gman59 <druckergm@gmail.com>
2018-11-10 18:41:56 -0800

    Added part 2 of passport

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var bodyParser = require('body-parser')
 var path = require('path')
 var fs = require('fs')
 var https = require('https')
-
+var passport = require('passport');
+var Auth0Strategy = require('passport-auth0');
 
 var app = express()
 var port = process.env.PORT || 3000
@@ -40,3 +41,27 @@ if (app.get('env') === 'production') {
 }
 
 app.use(session(sess));
+
+
+
+// Configure Passport to use Auth0
+var strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+app.use(passport.initialize());
+app.use(passport.session());
