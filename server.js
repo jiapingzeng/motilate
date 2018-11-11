c45f2705ba712ec5b46aefb4800f10fb0dc9302e
aaron <aahuang@berkeley.edu>
2018-11-10 22:46:30 -0800

    accident

--- server.js
+++ server.js
@@ -3,88 +3,42 @@ var bodyParser = require('body-parser')
 var path = require('path')
 var fs = require('fs')
 var https = require('https')
-// var passport = require('passport');
-// var Auth0Strategy = require('passport-auth0');
-// var userInViews = require('./middleware/userInViews');
-// var authRouter = require('./routes/auth');
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-//pizza
+var session = require('express-session')
+var passport = require('passport')
+
+var Auth0Strategy = require('passport-auth0');
+var index = require('./routes/index')
+var event = require('./routes/event')
+var user = require('./routes/user')
+require('./auth.js')
+
 var app = express()
 var port = process.env.PORT || 3000
-var session = require('express-session');
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.get('/user', (req, res) => {
-    res.render('dash')
-})
-app.get('/', (req, res) => {
-    res.render('index')
-})
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(session({
+  secret: 'very secret secret',
+  cookie: {
+    secure: true
+  },
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use('/', index)
+app.use('/event', event)
+app.use('/user', user)
 
 https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-  }, app)
-  .listen(port, function () {
-    console.log(`Example app listening on port ${port}! Go to https://localhost:3000/`)
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app).listen(port, function () {
+  console.log(`Example app listening on port ${port}! Go to https://localhost:3000/`)
 })
-
-// config express-session
-// var sess = {
-//   secret: 'codeDaySecert2018Fall',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true
-// };
-
-// if (app.get('env') === 'production') {
-//   sess.cookie.secure = true; // serve secure cookies, requires https
-// }
-
-// app.use(session(sess));
-
-
-
-// // Configure Passport to use Auth0
-// var strategy = new Auth0Strategy(
-//   {
-//     domain: process.env.AUTH0_DOMAIN,
-//     clientID: process.env.AUTH0_CLIENT_ID,
-//     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-//     callbackURL:
-//       process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
-//   },
-//   function (accessToken, refreshToken, extraParams, profile, done) {
-//     // accessToken is the token to call Auth0 API (not needed in the most cases)
-//     // extraParams.id_token has the JSON Web Token
-//     // profile has all the information from the user
-//     return done(null, profile);
-//   }
-// );
-
-// passport.use(strategy);
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// passport.serializeUser(function (user, done) {
-//   done(null, user);
-// });
-
-// passport.deserializeUser(function (user, done) {
-//   done(null, user);
-// });
-
-// // app.js
-
-// // ..
-// app.use(userInViews());
-// app.use('/', authRouter);
-// app.use('/', indexRouter);
-// app.use('/', usersRouter);
-// // ..
