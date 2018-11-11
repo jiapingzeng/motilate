d818240212b63e579d21bd6bb654ea5d09157792
Jiaping Zeng <jiaping@gisar.me>
2018-11-11 01:53:59 -0800

    finally?

--- auth.js
+++ auth.js
@@ -5,9 +5,17 @@ var strategy = new Auth0Strategy({
     domain: 'codeday.auth0.com',
     clientID: 'pu0iuM0f0FiE7wf7B5Y8xtl2bP4d9SH4',
     clientSecret: 'kccuUk5H90cMhkNhDxmRRCRS7l2Ra4AvnaXx6OKpMKW4NGJ_9H96cqCHFORc6H94',
-    callbackURL: 'http://localhost:3000/callback'
+    callbackURL: 'https://localhost:3000/user/callback'
 }, (accessToken, refreshToken, extraParams, profile, done) => {
     return done(null, profile)
 })
 
 passport.use(strategy)
+
+passport.serializeUser((user, done) => {
+    done(null, user)
+})
+
+passport.deserializeUser((user, done) => {
+    done(null, user)
+})
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var passport = require('passport')
+var path = require('path')
+var loggedIn = require('../lib/middleware/loggedIn')
 var router = express.Router()
 
 router.get('/login', passport.authenticate('auth0', {
@@ -8,20 +10,28 @@ router.get('/login', passport.authenticate('auth0', {
     res.redirect('/')
 })
 
+router.get('/profile', loggedIn(), (req, res, next) => {
+    const { _raw, _json, ...userProfile } = req.user
+    res.render('user/profile', {
+        userProfile: JSON.stringify(userProfile, null, 2),
+        title: 'Profile'
+    })
+})
+
 router.get('/callback', (req, res, next) => {
     passport.authenticate('auth0', (err, user, info) => {
         if (err) return next(err)
-        if (!user) return res.redirect('/login')
-        req.login(user, (err) => {
+        if (!user) return res.redirect('/user/login')
+        req.logIn(user, (err) => {
             if (err) return next(err)
             const returnTo = req.session.returnTo
             delete req.session.returnTo
-            res.redirect(returnTo || '/user')
+            res.redirect(returnTo || '/user/profile')
         })
     })(req, res, next)
 })
 
-router.get('logout', (req, res) => {
+router.get('/logout', (req, res) => {
     req.logout()
     res.redirect('/')
 })
