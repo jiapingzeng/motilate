34a776b0571e1ee1e745cea81b5787547674c036
someguy13 <ryan.kwong@gmail.com>
2018-11-11 07:34:42 -0800

    a

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.redirect('/login')
 })
 
 router.get('/create', (req, res) => {
