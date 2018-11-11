22638e8204ebd2cef9d7d3469c1f38b0289cabe1
Jiaping Zeng <jiaping@gisar.me>
2018-11-11 06:57:50 -0800

    afs

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ router.get('/', (req, res) => {
     res.render('index')
 })
 
+router.get('/create', (req, res) => {
+    res.redirect('/event/create')
+})
+
 router.get('/login', (req, res) => { 
     res.redirect('/user/login')
 })
