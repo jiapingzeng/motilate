f4a9f1829bf41a2599f2491a8e3958fb69d70a23
Jiaping Zeng <jiaping@gisar.me>
2018-11-11 07:21:25 -0800

    asdf

--- routes/event.js
+++ routes/event.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var router = express.Router()
 
+router.get('/calendar', (req, res) => {
+    res.render('event/calendar')
+})
+
 router.get('/create', (req, res) => {
     res.render('event/create')
 })
